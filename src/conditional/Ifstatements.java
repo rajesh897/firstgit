b9706493cf37c83d91fdd9ab6758ce6922d8158d
rajesh897 <rajsircrr@gmail.com>
2021-12-11 18:47:06 +0530

    git first program

---
+++ src/conditional/Ifstatements.java
@@ -0,0 +1,18 @@
+package conditional;
+
+public class Ifstatements {
+	public static void main(String[] args) {
+		
+	int age = 18;
+	if (age>18) {
+		System.out.println("eligiable for vote");
+	}
+	System.out.println("not eligiable for vote");
+		
+	}
+}
+	
+	
+	
+	
+
